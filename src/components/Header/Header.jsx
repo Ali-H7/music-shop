097df51cf24e29d4705ab48482df96071caf681b
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-18 04:50:37 +0300

    Add styling to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import './Header.css';
+import styles from './Header.module.css';
 
 function Header() {
   return (
-    <header>
-      <h1>Music Shop</h1>
-      <nav>
-        <ul>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <h1 className={styles.logo}>Music Shop</h1>
+        <ul className={styles.list}>
           <li>Home</li>
           <li>Shop</li>
           <li>Cart</li>
@@ -14,3 +14,5 @@ function Header() {
     </header>
   );
 }
+
+export default Header;
