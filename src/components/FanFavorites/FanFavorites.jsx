7be05b54bd3de33f523029b896979231410d11fb
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-18 08:55:10 +0300

    Place button to add items to cart

--- src/components/FanFavorites/FanFavorites.jsx
+++ src/components/FanFavorites/FanFavorites.jsx
@@ -1,4 +1,5 @@
 import styles from './FanFavorites.module.css';
+import { Plus, Minus } from 'lucide-react';
 import { useEffect, useState } from 'react';
 
 function FanFavorites() {
@@ -25,6 +26,14 @@ function FanFavorites() {
                 <p>{album.price}</p>
               </div>
               <p className={styles.artistName}>{album.artist}</p>
+              <div className='add-to-cart'>
+                <div className={styles.count}>
+                  <Minus />
+                  <p>0</p>
+                  <Plus />
+                </div>
+                <button className={styles.cartButton}>Add to Cart</button>
+              </div>
             </div>
           );
         })}
