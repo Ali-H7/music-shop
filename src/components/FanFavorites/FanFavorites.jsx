5a430401aa89bea6718b1cd17ed577013406b0f1
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-18 08:34:36 +0300

    Add styling to FanFavorites component

--- src/components/FanFavorites/FanFavorites.jsx
+++ src/components/FanFavorites/FanFavorites.jsx
@@ -14,17 +14,21 @@ function FanFavorites() {
 
   return (
     <div className={styles.fanFavorites}>
-      <h2>Fan Favorites</h2>
-      {albums.map((album) => {
-        return (
-          <div key={album.id} className={styles.albumCard}>
-            <img src={album.image} />
-            <h3>{album.albumName}</h3>
-            <p>{album.artist}</p>
-            <p>{album.price}</p>
-          </div>
-        );
-      })}
+      <h2 className={styles.title}>Fan Favorites</h2>
+      <div className={styles.cards}>
+        {albums.map((album) => {
+          return (
+            <div key={album.id} className={styles.albumCard}>
+              <img src={album.image} className={styles.albumCover} />
+              <div className={styles.albumTitle}>
+                <h3>{album.albumName}</h3>
+                <p>{album.price}</p>
+              </div>
+              <p className={styles.artistName}>{album.artist}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
