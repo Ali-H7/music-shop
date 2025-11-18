898d5a30acdfc1d33ddb5fe75c9601ac558dcf1e
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-18 04:56:14 +0300

    Create announcement banner component
    Text displayed on this component will be used for offers or new items.

---
+++ src/components/AnnouncementBanner/AnnouncementBanner.jsx
@@ -0,0 +1,11 @@
+import styles from './AnnouncementBanner.module.css';
+
+function AnnouncementBanner() {
+  return (
+    <div>
+      <p>Free shipping for all order over £25 and just a reminder, all our deliveries are carbon neutral</p>
+    </div>
+  );
+}
+
+export default AnnouncementBanner;
