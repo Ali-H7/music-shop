0f09177880e767e181b509a10fbb7b3d3b6425fb
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-19 04:36:21 +0300

    Create Newsletter component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AnnouncementBanner from './components/AnnouncementBanner/AnnouncementBann
 import FanFavorites from './components/FanFavorites/FanFavorites.jsx';
 import AlbumOfTheMonth from './components/AlbumOfTheMonth/AlbumOfTheMonth.jsx';
 import Category from './components/Category/Category.jsx';
+import Newsletter from './components/Newsletter/Newsletter.jsx';
 import { useEffect, useState } from 'react';
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
       <FanFavorites albums={albums} />
       <AlbumOfTheMonth album={albumOfTheMonth} />
       <Category />
+      <Newsletter />
     </div>
   );
 }
---
+++ src/components/Newsletter/Newsletter.jsx
@@ -0,0 +1,21 @@
+import styles from './Newsletter.module.css';
+
+function Newsletter() {
+  return (
+    <div className={styles.newsletter}>
+      <p>Discover hidden gems, collector's editions, and subscriber-only deals.</p>
+      <div className={styles.container}>
+        <div className={styles.emailContainer}>
+          <input type='email' placeholder='Enter Email Addresss' className={styles.emailInput} />
+          <button className={styles.signUpBtn}>Sign Up</button>
+        </div>
+        <div className={styles.agreementContainer}>
+          <input type='checkbox' />
+          <p>Yes, spin me into your mailing list for offers and music news.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Newsletter;
