b1a6c45e20b8635c4daffe03d41b2e32f3243ff3
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-20 04:26:29 +0300

    Add root link to the home button
    Link used to navigate to the homepagel.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import styles from './Header.module.css';
-
+import { Link } from 'react-router';
 function Header() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <h1 className={styles.logo}>Music Shop</h1>
         <ul className={styles.list}>
-          <li>Home</li>
+          <Link to='/'>
+            <li>Home</li>
+          </Link>
           <li>Shop</li>
           <li>Cart</li>
         </ul>
