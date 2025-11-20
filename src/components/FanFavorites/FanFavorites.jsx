4154455c7ba61439299815aab3edd97994c848ca
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-20 09:08:42 +0300

    Change the layout background color
    New color will make it easier on the eyes.

--- src/components/FanFavorites/FanFavorites.jsx
+++ src/components/FanFavorites/FanFavorites.jsx
@@ -16,7 +16,7 @@ function FanFavorites({ albums }) {
                 <Link to={`/product/${album.id}`}>
                   <h3 className={styles.ablumName}>{album.albumName}</h3>
                 </Link>
-                <p className={styles.price}>{album.price}</p>
+                <p className={styles.price}>{`${album.price} BHD`}</p>
               </div>
               <p className={styles.artistName}>{album.artist}</p>
             </div>
