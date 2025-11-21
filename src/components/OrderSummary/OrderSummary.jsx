3ad1ae03b108dea52a2ccf418931fad4052aabe7
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-21 14:21:46 +0300

    Add calculation in the cart page
    now items on the cart get calculated and the price displayed.

--- src/components/OrderSummary/OrderSummary.jsx
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -1,24 +1,36 @@
 import styles from './OrderSummary.module.css';
-
+import { useOutletContext } from 'react-router';
 function OrderSummary() {
+  const { cart, albums } = useOutletContext();
+
+  const subTotal = cart
+    .map((itemReference) => {
+      const album = albums.find((item) => item.id === itemReference.id);
+      return album.price * itemReference.quantity;
+    })
+    .reduce((a, b) => a + b, 0);
+  const shipping = cart.map((item) => item.quantity).reduce((a, b) => a + b, 0) * 0.2;
+  const vat = subTotal * 0.1;
+  const total = subTotal + shipping + vat;
+
   return (
     <div className={styles.summaryContainer}>
       <h3 className={styles.title}>Order Summary</h3>
       <div className={styles.price}>
         <p>Subtotal:</p>
-        <p>1 BHD</p>
+        <p>{`${subTotal.toFixed(3)} BHD`}</p>
       </div>
       <div className={styles.price}>
         <p>Shipping:</p>
-        <p>1 BHD</p>
+        <p>{`${shipping.toFixed(3)} BHD`}</p>
       </div>
       <div className={styles.vat}>
         <p>VAT</p>
-        <p>1 BHD</p>
+        <p>{`${vat.toFixed(3)} BHD`}</p>
       </div>
       <div className={styles.total}>
         <p>Total:</p>
-        <p>1 BHD</p>
+        <p>{`${total.toFixed(3)} BHD`}</p>
       </div>
       <button className={styles.checkoutBtn}>Checkout</button>
       <p className={styles.continue}>Continue Shopping</p>
