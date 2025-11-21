b4d5692c228c72cb47d452e3232acde64d163fb3
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-21 13:47:14 +0300

    Create CartPopup component

---
+++ src/components/CartPopup/CartPopup.jsx
@@ -0,0 +1,40 @@
+import styles from './CartPopup.module.css';
+import { useOutletContext } from 'react-router';
+import Quantity from '../Quantity/Quantity';
+import { SquareX } from 'lucide-react';
+import OrderSummary from '../OrderSummary/OrderSummary';
+function CartPopup({ handleCartPopUp }) {
+  const { albums, cart } = useOutletContext();
+  return (
+    <div className={styles.module}>
+      <div className={styles.title}>
+        <h3>Added to cart</h3>
+        <button className={styles.closeBtn} onClick={handleCartPopUp}>
+          <SquareX />
+        </button>
+      </div>
+
+      <div className={styles.products}>
+        {cart.map((itemRef) => {
+          const { id } = itemRef;
+          const album = albums.find((item) => item.id === id);
+          return (
+            <div key={album.id} className={styles.product}>
+              <img className={styles.cover} src={album.image} alt='Album Cover' />
+              <div className={styles.productInfo}>
+                <p>{album.albumName}</p>
+                <p className={styles.subText}>{`${album.price} BHD`}</p>
+                <Quantity id={album.id} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.summary}>
+        <OrderSummary />
+      </div>
+    </div>
+  );
+}
+
+export default CartPopup;
