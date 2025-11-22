f563e6b8938c1a2b573fe3466a946f79fcb02b19
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-22 12:59:28 +0300

    Add github logo in the banner

--- src/components/Copyrights/Copyrights.jsx
+++ src/components/Copyrights/Copyrights.jsx
@@ -1,14 +1,14 @@
 import styles from './Copyrights.module.css';
-
 function Copyrights() {
   return (
     <div className={styles.copyrights}>
       <p>
         Copyright © 2025, Developed & designed by{' '}
-        <a href='https://github.com/Ali-H7'>
+        <a href='https://github.com/Ali-H7' target='_blank'>
           <span className={styles.name}>Ali Hasan</span>
         </a>
       </p>
+      <i className={`fa fa-github ${styles.icon}`} aria-hidden='true'></i>
     </div>
   );
 }
