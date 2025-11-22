54d72b3317277b70e2eaa25a2cb4aaad90e90ed7
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-22 17:20:42 +0300

    Make Header Responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,46 @@
 import styles from './Header.module.css';
 import { Link } from 'react-router';
+import { Menu, SquareX } from 'lucide-react';
+import { useState } from 'react';
 function Header({ quantity }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <h1 className={styles.logo}>Music Shop</h1>
-        <ul className={styles.list}>
-          <Link to='/'>
-            <li>Home</li>
-          </Link>
-          <Link to='/shop/'>
-            <li>Shop</li>
-          </Link>
-          <div className={styles.cartContainer}>
-            <Link to='/cart/'>
-              <li>Cart</li>
-            </Link>
-            <div className={`${styles.qunatity} ${quantity > 0 && styles.show}`}>
-              <p>{quantity}</p>
-            </div>
+        <div className={styles.menu}>
+          <button className={styles.button} onClick={toggleMenu}>
+            <Menu size={32} />
+          </button>
+        </div>
+        <div className={`${styles.listContainer} ${isMenuOpen && styles.show}`}>
+          <div className={styles.closeIcon}>
+            <button className={styles.button} onClick={toggleMenu}>
+              <SquareX size={32} />
+            </button>
           </div>
-        </ul>
+          <ul className={styles.list}>
+            <Link to='/'>
+              <li>Home</li>
+            </Link>
+            <Link to='/shop/'>
+              <li>Shop</li>
+            </Link>
+            <div className={styles.cartContainer}>
+              <Link to='/cart/'>
+                <li>Cart</li>
+              </Link>
+              <div className={`${styles.qunatity} ${quantity > 0 && styles.show}`}>
+                <p>{quantity}</p>
+              </div>
+            </div>
+          </ul>
+        </div>
       </nav>
     </header>
   );
