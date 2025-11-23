e974591bf38600af1101d1f2c855e3d92c004c02
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-23 09:51:35 +0300

    Make Copyrights responsive

--- src/components/Copyrights/Copyrights.jsx
+++ src/components/Copyrights/Copyrights.jsx
@@ -2,13 +2,14 @@ import styles from './Copyrights.module.css';
 function Copyrights() {
   return (
     <div className={styles.copyrights}>
-      <p>
-        Copyright © 2025, Developed & designed by{' '}
+      <p>Copyright © 2025, Developed & designed by</p>
+
+      <div className={styles.nameContainer}>
         <a href='https://github.com/Ali-H7' target='_blank'>
           <span className={styles.name}>Ali Hasan</span>
         </a>
-      </p>
-      <i className={`fa fa-github ${styles.icon}`} aria-hidden='true'></i>
+        <i className={`fa fa-github ${styles.icon}`} aria-hidden='true'></i>
+      </div>
     </div>
   );
 }
