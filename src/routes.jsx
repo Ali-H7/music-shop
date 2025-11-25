9dcee36f3208b6f189df44c31213746b0da58232
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-25 16:31:02 +0300

    Reset scroll to top of the page when navigating

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,30 @@ import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Shop from './pages/Shop';
 import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
+import { useLayoutEffect } from 'react';
+import { useLocation } from 'react-router';
+
+//sloution found (https://medium.com/@caden0002/fixing-scroll-position-issues-in-react-router-scroll-to-top-on-navigation-86bcfbdfc9db)
+// credit to them
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    // Scroll to the top of the page when the route changes
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+  }, [location.pathname]);
+
+  return children;
+};
+
 const routes = [
   {
     path: '/',
-    element: <App />,
+    element: (
+      <Wrapper>
+        <App />
+      </Wrapper>
+    ),
     children: [
       {
         index: true,
